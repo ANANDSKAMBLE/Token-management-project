199aa59ed94478d6c6b012f4317d81f5e485f953
ANANDSKAMBLE <149240693+ANANDSKAMBLE@users.noreply.github.com>
2024-06-04 16:44:14 +0530

    server.js

--- booking_p2/server.js
+++ booking_p2/server.js
@@ -1,6 +1,6 @@
 var http= require('http');
 const app = require('./app.js');
-// const app = require('C:/Users/Anand S Kamble/Desktop/html/booking_p2/app.js');
+
 const port = 3030;
 
 const server = http.createServer(app);
